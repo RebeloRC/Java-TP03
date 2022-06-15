4174046bdb6cce2ddc0d4c683e992dad25514410
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-06-15 13:31:06 -0300

    task completed

---
+++ src/teste/Main.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+        Hora Horario = new Hora();
+        System.out.println(Horario.getHora1());
+        System.out.println(Horario.getHora2());
+        System.out.println(Horario.getSegundos());
+		
+	}
+}
